40173db7ef74659a5561b0c4312df99fd695f1c6
Toby <40096033+tobytoyin@users.noreply.github.com>
2024-05-11 11:09:25 +0100

    add telescope keymapping

--- mapping.lua
+++ mapping.lua
@@ -2,3 +2,15 @@
 -- <leader> is whitespace keys
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+
+
+-- [[ 
+-- Telescope Keymapping 
+-- ]]
+
+local builtin = require("telescope.builtin")
+
+-- Navigation mode
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
