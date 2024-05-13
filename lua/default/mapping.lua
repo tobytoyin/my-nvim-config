05e3b941bc666910239b895c89ff0f9d975a86fa
Toby <40096033+tobytoyin@users.noreply.github.com>
2024-05-13 22:40:34 +0100

    add system clipboard copy & paste

--- lua/default/mapping.lua
+++ lua/default/mapping.lua
@@ -8,6 +8,9 @@ vim.keymap.set("n", "<leader>vs", ":vsplit<cr>", {noremap = true})
 vim.keymap.set("n", "<leader>to", ":vsplit | terminal<cr>", {noremap = true})
 
 vim.keymap.set("n", "<leader>ws", ":%s/") --word-substitue
+vim.keymap.set("n", "<leader>yy", '"*yy') -- clipboard copy
+vim.keymap.set("n", "<leader>pp", '"*p')  -- clipboard paste
+
 
 -- remapping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
